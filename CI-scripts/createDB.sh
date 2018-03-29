0059e979859ae0974f2a98f170dc30d3ca8fdf0a
Vojtěch Stuchlík <vojtech.stuchlik@blogic.cz>
2018-03-30 00:38:32 +0200

    creating .env for testing

--- CI-scripts/createDB.sh
+++ CI-scripts/createDB.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ ! -f ".env" ]; then
+    cp .env.example .env
+    sed -i 's|^APP_ENV=.*$|APP_ENV=testing|' .env
+fi
+
 php artisan migrate --database=sqlite
 php artisan db:seed --database=sqlite
